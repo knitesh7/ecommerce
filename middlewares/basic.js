6c01d6ea7912b15d46852f58610bc57934409182
Nitesh Kumar <niteshstudy007@gmail.com>
2023-06-23 00:03:30 +0530

    fixed large payload due to imageBase64 issue

--- middlewares/basic.js
+++ middlewares/basic.js
@@ -3,8 +3,8 @@ const cors = require('cors')
 const bodyParser = require('body-parser');
 function basicMiddlewares(app) {
     app.use(cors())
-    app.use(express.json())
-    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json({ limit: '50mb' }));
+    app.use(express.urlencoded({ limit: '50mb', extended: true }));
     // Configure body-parser middleware to handle plain text
     app.use(bodyParser.text({ type: 'text/plain' }));
 }
